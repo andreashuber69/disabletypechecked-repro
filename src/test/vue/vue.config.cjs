577b38e143e6ee729762a7b2d3529ecb3e3d91e8
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-20 16:34:56 +0100

    chore: add cjs file

---
+++ src/test/vue/vue.config.cjs
@@ -0,0 +1,12 @@
+// https://github.com/andreashuber69/async-css-plugin/blob/develop/README.md#----async-css-plugin
+
+if (!require.extensions[".ts"]) {
+    require("ts-node").register({
+        project: "./tsconfig.json",
+        compilerOptions: {
+            module: "commonjs",
+        },
+    });
+}
+
+module.exports = require("./vue.config.cjs").default;
