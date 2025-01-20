d2984cfbe51b08e2160a9fbd1a169d3d196f4621
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-20 15:30:10 +0100

    chore: extend eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
-// @ts-check
+import config from "@andreashuber69/eslint-config";
+import tseslint from "typescript-eslint";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
+// eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+    config,
+    {
+        ignores: ["dist/"],
+    },
 );
