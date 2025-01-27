88e063b5abfba7f1272a696cea5f0b5179a16331
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-27 13:19:36 +0100

    chore: fix include & exclude paths

--- compiled.vitest.config.ts
+++ compiled.vitest.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "vitest/config";
 // eslint-disable-next-line import/no-anonymous-default-export, import/no-default-export
 export default defineConfig({
     test: {
+        include: ["dist/*.spec.js"],
+        exclude: ["coverage", "node_modules"],
         coverage: {
             provider: "istanbul",
-            include: ["dist/node/*.js"],
             reporter: ["lcov", "text"],
         },
     },
