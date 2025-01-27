3970341a30704741876eeeb92220d410aab4f865
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-27 14:02:11 +0100

    test: fix

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -8,8 +8,8 @@ describe("calculate", () => {
     });
 
     it("should add values", () => {
-        expect(add(1, 2) === 3);
-        expect(add(1, -2) === -1);
-        expect(add(1, 2, 3, 4, 5) === 15);
+        expect(add(1, 2)).toEqual(3);
+        expect(add(1, -2)).toEqual(-1);
+        expect(add(1, 2, 3, 4, 5)).toEqual(15);
     });
 });
