360213e7c3483ef76968bee0680f766e808c8e90
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-02-01 21:35:30 +0100

    test: import compiled code into test sources

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { add } from "./add.ts";
+import { add } from "../dist/index.js";
 
 describe("calculate", () => {
     it("should return 0 when there are no values", () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // eslint-disable-next-line import/no-default-export, import/no-anonymous-default-export
 export default defineConfig({
     build: {
         lib: {
-            entry: ["src/add.spec.ts"],
+            entry: ["src/index.ts"],
             formats: ["es"],
         },
         outDir: "dist",
         rollupOptions: {
-            input: ["src/add.spec.ts"],
+            input: ["src/index.ts"],
         },
         sourcemap: true,
         ssr: true,
     },
+    plugins: [dts({ rollupTypes: true })],
 });
